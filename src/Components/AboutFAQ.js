f171dc99a5a2ef39510195a397c8fcec1b0a3ec5
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2020-12-02 19:14:26 +0100

    FAQ Section...

--- src/Components/AboutFAQ.js
+++ src/Components/AboutFAQ.js
@@ -2,8 +2,44 @@ import React from 'react'
 
 const AboutFAQ = () => {
  return (
-  <div>
-   dfdsf
+  <div class="about__faq">
+   <h2>Any question <span>FAQ</span></h2>
+   <div className="question">
+    <h4>How Do I Start?</h4>
+    <div className="answer">
+     <p>sadsadasd</p>
+     <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos dignissimos veritatis voluptatum aliquam beatae quidem non maiores totam. Maiores iusto eius nisi, enim facere quidem fugiat quo. Sapiente, quis illum?
+     </p>
+    </div>
+   </div>
+   <div className="question">
+    <h4>Daily Schedule</h4>
+    <div className="answer">
+     <p>sadsadasd</p>
+     <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos dignissimos veritatis voluptatum aliquam beatae quidem non maiores totam. Maiores iusto eius nisi, enim facere quidem fugiat quo. Sapiente, quis illum?
+     </p>
+    </div>
+   </div>
+   <div className="question">
+    <h4>My Work experince</h4>
+    <div className="answer">
+     <p>sadsadasd</p>
+     <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos dignissimos veritatis voluptatum aliquam beatae quidem non maiores totam. Maiores iusto eius nisi, enim facere quidem fugiat quo. Sapiente, quis illum?
+     </p>
+    </div>
+   </div>
+   <div className="question">
+    <h4>What product do you offer.</h4>
+    <div className="answer">
+     <p>sadsadasd</p>
+     <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos dignissimos veritatis voluptatum aliquam beatae quidem non maiores totam. Maiores iusto eius nisi, enim facere quidem fugiat quo. Sapiente, quis illum?
+     </p>
+    </div>
+   </div>
   </div>
  )
 }
