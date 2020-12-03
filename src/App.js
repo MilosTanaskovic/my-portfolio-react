7f1cf3101175a5826f80c93937d202ced552fb5a
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2020-12-03 18:08:45 +0100

    styled components....

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+// Global Style
+import GlobalStyle from './Components/GlobalStyle';
 import './App.css';
 // import pages
 import AboutMe from './Pages/AboutMe';
 function App() {
   return (
     <div className="App">
+      <GlobalStyle/>
       <h1>Dancing With The React!!!</h1>
       <AboutMe />
     </div>
--- src/Components/AboutSection.js
+++ src/Components/AboutSection.js
@@ -1,21 +1,63 @@
 import React from 'react'
 // import img
 import Img from '../img/About/MilosTanaskovic.jpg';
+// Styled
+import styled from 'styled-components';
 const AboutSection = () => {
  return (
-  <div className="about__section">
-   <div className="description">
+  <Styled_About>
+    <Styled_Description>
     <div className="title">
-     title
+     <Styled_Hide>
+      <h2>We work to make</h2>
+     </Styled_Hide>
+     <Styled_Hide>
+      <h2>
+       your <span>dreams</span> come
+      </h2>
+     </Styled_Hide>
+     <Styled_Hide>
+      <h2>true.</h2>
+     </Styled_Hide>
     </div>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, corporis! Voluptas natus repellat, provident odit reprehenderit repellendus nihil dicta iusto laborum quibusdam aspernatur dolor adipisci doloribus explicabo sed unde. Esse.</p>
+    <p>
+     Conatct me for any task or proggraming idea have professional with amazing skills.
+    </p>
     <button>Contact Me</button>
-   </div>
-   <div className="image">
+   </Styled_Description>
+   <Styled_Image>
     <img src={Img} width={200} height={200} alt="Image Milos Tanaskovic"/>
-   </div>
-  </div>
+   </Styled_Image>
+  </Styled_About>
  )
 }
+// Styled Components
+const Styled_About = styled.div`
+ min-height: 90vh;
+ display: flex;
+ align-items: center;
+ justify-content: space-between;
+ padding: 5rem 10rem;
+ color: white;
+`;
+const Styled_Description = styled.div`
+ flex: 1;
+ padding-right: 5rem;
+ h2{
+  font-weight:lighter;
+ }
+`;
+const Styled_Image = styled.div`
+ flex: 1;
+ overflow: hidden;
+ img{
+  width: 100%;
+  height: 80vh;
+  object-fit: cover;
+ }
+`;
+const Styled_Hide = styled.div`
+ overflow: hidden;
+`;
 
 export default AboutSection
