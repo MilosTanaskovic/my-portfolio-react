b3546ecd757c47a51c59064a6691c430a5604b63
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2020-12-07 18:18:23 +0100

    styling faq section - styled-comonents....

--- src/Components/AboutFAQ.js
+++ src/Components/AboutFAQ.js
@@ -1,8 +1,11 @@
 import React from 'react'
+// import srtyled
+import styled from 'styled-components';
+import { Styled_About } from './StyledAbout';
 
 const AboutFAQ = () => {
  return (
-  <div class="about__faq">
+  <Styled_FAQ>
    <h2>Any question <span>FAQ</span></h2>
    <div className="question">
     <h4>How Do I Start?</h4>
@@ -12,6 +15,7 @@ const AboutFAQ = () => {
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos dignissimos veritatis voluptatum aliquam beatae quidem non maiores totam. Maiores iusto eius nisi, enim facere quidem fugiat quo. Sapiente, quis illum?
      </p>
     </div>
+    <div className="faq-line"></div>
    </div>
    <div className="question">
     <h4>Daily Schedule</h4>
@@ -21,6 +25,7 @@ const AboutFAQ = () => {
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos dignissimos veritatis voluptatum aliquam beatae quidem non maiores totam. Maiores iusto eius nisi, enim facere quidem fugiat quo. Sapiente, quis illum?
      </p>
     </div>
+    <div className="faq-line"></div>
    </div>
    <div className="question">
     <h4>My Work experince</h4>
@@ -30,6 +35,7 @@ const AboutFAQ = () => {
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos dignissimos veritatis voluptatum aliquam beatae quidem non maiores totam. Maiores iusto eius nisi, enim facere quidem fugiat quo. Sapiente, quis illum?
      </p>
     </div>
+    <div className="faq-line"></div>
    </div>
    <div className="question">
     <h4>What product do you offer.</h4>
@@ -40,8 +46,35 @@ const AboutFAQ = () => {
      </p>
     </div>
    </div>
-  </div>
+  </Styled_FAQ>
  )
 }
+
+const Styled_FAQ = styled(Styled_About)`
+ display: block;
+ span {
+  display: block;
+ }
+ h2{
+  padding-bottom: 2rem;
+  font-weight: lighter;
+ }
+ .faq-line{
+  background: #cccccc;
+  height: 0.2rem;
+  margin: 2rem 0rem;
+  width: 100%;
+ }
+ .question{
+  padding: 3rem 0rem;
+  cursor: pointer;
+ }
+ .answer{
+  padding: 2rem 0rem;
+  p{
+   padding: 1rem 0rem;
+  }
+ }
+`;
 
 export default AboutFAQ
